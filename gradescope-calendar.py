819e108792775446efc512d95c7f81e63d9bf1b2
CoolSpy3 <coolspythree@gmail.com>
2023-09-05 09:25:15 -0700

    initial commit

---
+++ gradescope-calendar.py
@@ -0,0 +1,55 @@
+import json
+import xml.etree.ElementTree as ET
+from datetime import datetime, timezone
+
+import requests
+from googleapiclient.discovery import build as buildGoogleAPIService
+
+import utils
+
+# Read in the config file
+gradescope_token = None
+
+with open('gradescope_secrets.json') as f:
+    gradescope_token = json.load(f)['gradescope_token']
+
+gradescope_courses = [
+    (
+        utils.transform_or_default(course.find("./h3"), lambda course_name: course_name.text, "<Unknown Course>").strip(),
+        course.attrib["href"]
+    )
+    for course in utils.get_data_from_gradescope("", ".//div[@class='courseList']/div[@class='courseList--coursesForTerm'][2]/a[@class='courseBox']", gradescope_token)
+]
+gradescope_assignments = {
+    course_name: [
+        {
+            "name": utils.get_assignment_name(assignment),
+            "due_date": utils.due_date_from_progress_div(assignment[2][0][2]),
+            "completed": assignment[1][1].text == "Submitted"
+        }
+        for assignment in utils.get_data_from_gradescope(course_url, ".//table[@id='assignments-student-table']/tbody/tr", gradescope_token)
+        if len(assignment[2][0]) > 1 # If the assignment is past due, Gradescope will not include a progress bar div
+    ]
+    for course_name, course_url in gradescope_courses
+}
+gradescope_assignments = {
+    course_name: [
+        assignment for assignment in course_assignments if isinstance(assignment, dict) and assignment["due_date"]["normal"]
+    ]
+    for course_name, course_assignments in gradescope_assignments.items()
+}
+
+with buildGoogleAPIService('calendar', 'v3', credentials=utils.login_with_google()) as calendar_service:
+    gradescope_calendar_id = utils.find_gradescope_calendar_id(calendar_service) or utils.create_gradescope_calendar(calendar_service)
+
+    calendar_events = list(utils.enumerate_calendar_events(calendar_service, gradescope_calendar_id))
+
+    for course_name, course_url in gradescope_courses:
+        for assignment in gradescope_assignments[course_name]:
+            calendar_event = utils.get_assignment_in_calendar(assignment, calendar_events)
+
+            if not calendar_event:
+                if assignment["due_date"]["normal"] > datetime.now(timezone.utc):
+                    utils.create_assignment_event(calendar_service, gradescope_calendar_id, course_name, course_url, assignment)
+            elif assignment["completed"] and calendar_event["status"] != "cancelled":
+                calendar_service.events().patch(calendarId=gradescope_calendar_id, eventId=calendar_event["id"], body={"status": "cancelled"}).execute()
---
+++ utils.py
@@ -0,0 +1,137 @@
+import os
+import xml.etree.ElementTree as ElementTree
+from datetime import datetime
+
+import requests
+import tzlocal
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+CALENDAR_DESCRIPTION = "<USE AS GRADESCOPE CALENDAR>"
+GRADESCOPE_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S %z"
+
+#region Google Calendar
+
+def login_with_google():
+    google_API_scopes = ['https://www.googleapis.com/auth/calendar.calendarlist', 'https://www.googleapis.com/auth/calendar.calendars', 'https://www.googleapis.com/auth/calendar.events']
+
+    # Copied from Google's API documentation
+    google_credentials = None
+    if os.path.exists('google_token.json'):
+        google_credentials = Credentials.from_authorized_user_file('google_token.json', google_API_scopes)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not google_credentials or not google_credentials.valid:
+        if google_credentials and google_credentials.expired and google_credentials.refresh_token:
+            google_credentials.refresh(Request())
+        else:
+            oauthFlow = InstalledAppFlow.from_client_secrets_file('./google_secrets.json', scopes=google_API_scopes)
+            google_credentials = oauthFlow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('google_token.json', 'w') as token:
+            token.write(google_credentials.to_json())
+
+    return google_credentials
+
+def enumerate_calendar_events(calendar_service, calendar_id):
+    page_token = None
+    while True:
+        events = calendar_service.events().list(calendarId=calendar_id, pageToken=page_token).execute()
+        for event in events["items"]:
+            yield event
+        page_token = events.get("nextPageToken")
+        if not page_token:
+            break
+
+#endregion
+
+#region Gradescope
+
+def format_gradescope_url(url):
+    return f'https://www.gradescope.com{url if url.startswith("/") else f"/{url}"}'
+
+def get_data_from_gradescope(url, query, gradescope_token):
+    gradescope_cookies = { "signed_token": gradescope_token }
+    with requests.get(format_gradescope_url(url), cookies=gradescope_cookies) as response:
+        if response.status_code != 200:
+            print("Gradescope Error!")
+            print(response.content)
+            exit(1)
+
+        return ElementTree.fromstring(response.content).findall(query)
+
+def transform_or_default(data, transform, default):
+    return default if data == None else transform(data)
+
+#endregion
+
+#region Calendar
+
+def is_gradescope_calendar(calendar) -> bool:
+    if calendar["accessRole"] != "owner" or calendar.get("deleted", False):
+        return False
+    return CALENDAR_DESCRIPTION in calendar["description"] if "description" in calendar else False
+
+def create_gradescope_calendar(calendar_service):
+    calendar = {
+        "summary": "Gradescope Assignments",
+        "description": CALENDAR_DESCRIPTION,
+        "timeZone": str(tzlocal.get_localzone())
+    }
+    created_calendar = calendar_service.calendars().insert(body=calendar).execute()
+    return created_calendar["id"]
+
+def find_gradescope_calendar_id(calendarService):
+    page_token = None
+    while True:
+        calendar_list = calendarService.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list["items"]:
+            if is_gradescope_calendar(calendar_list_entry):
+                return calendar_list_entry["id"]
+        page_token = calendar_list.get("nextPageToken")
+        if not page_token:
+            break
+
+    return page_token
+
+#endregion
+
+#region Assignments
+
+def get_assignment_in_calendar(assignment, calendar_events):
+    for event in calendar_events:
+        if event["summary"] == assignment["name"] and datetime.fromisoformat(event["start"]["dateTime"]) == assignment["due_date"]["normal"]: # and datetime.fromisoformat(event["end"]["dateTime"]) == assignment["due_date"]["late"]
+            return event
+
+    return None
+
+def create_assignment_event(calendar_service, gradescope_calendar_id, course_name, course_url, assignment):
+    event = {
+        "summary": assignment["name"],
+        "description": f'Assignment for <a href="{format_gradescope_url(course_url)}">{course_name}</a> on Gradescope',
+        "start": {
+            "dateTime": assignment["due_date"]["normal"].isoformat()
+        },
+        "end": {
+            "dateTime": assignment["due_date"]["normal"].isoformat()
+            # "dateTime": assignment["due_date"]["late"].isoformat()
+        },
+        "status": "cancelled" if assignment["completed"] else "tentative",
+        "transparency": "transparent"
+    }
+    calendar_service.events().insert(calendarId=gradescope_calendar_id, body=event).execute()
+
+
+def get_assignment_name(assignment):
+    assignment_name = assignment.find("./th")
+    return transform_or_default(assignment_name[0] if len(assignment_name) > 0 else assignment_name, lambda assignment_name: assignment_name.text, "<Unknown Assignment>").strip()
+
+def due_date_from_progress_div(progress_div):
+    times = progress_div.findall("./time")
+    return {
+        "normal": datetime.strptime(times[1].get("datetime"), GRADESCOPE_DATETIME_FORMAT),
+        "late": datetime.strptime(times[2].get("datetime"), GRADESCOPE_DATETIME_FORMAT) if len(times) > 2 else None
+    }
+
+#endregion
